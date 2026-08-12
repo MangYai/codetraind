ae926f22da5bbd5423f9db9ecde2582a5735a648
MangYai <zsajkllam@gmail.com>
2026-08-12 21:13:54 +0700

    pronew

---
+++ Problem07.py
@@ -0,0 +1,19 @@
+def prime_numbers_in_range(start: int, end: int) -> tuple:
+    if start > end:
+        return ([], 0)
+    
+    is_prime_list = [True] * (end + 1)
+    is_prime_list[0] = False
+    if end >= 1:
+        is_prime_list[1] = False
+        
+    for i in range(2, int(end ** 0.5) + 1):
+        if is_prime_list[i]:
+            for j in range(i * i,end + 1 , i):
+                is_prime_list[j] = False
+                
+    primes = [n for n in range(start, end + 1)if is_prime_list[n]]
+    
+    return(primes, sum(primes))
+
+print(prime_numbers_in_range(10,20))
