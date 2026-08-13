16b5031ad3e008b2970cd9f50d25cce6e25e1e2b
MangYai <zsajkllam@gmail.com>
2026-08-14 03:26:21 +0700

    trainding

---
+++ Problem08.py
@@ -0,0 +1,10 @@
+from typing import List
+def average_length_of_strings(strings: List[str]) -> float:
+    total_length = 0
+    for s in strings:
+        total_length += len(s)
+    average_length = total_length / len(strings)
+    return average_length
+        
+word = ["apple", "banana", "cherry", "date", "elderberry"]    
+print(average_length_of_strings(word))
--- yyry.py
+++ yyry.py
@@ -1,1 +1,18 @@
-print("POLHOGO")
+def is_armstrong(number):
+    text = str(number)
+    digit_count = len(text)
+    total = 0
+    
+    for char in text:
+        digit = int(char)
+        total += digit ** digit_count
+        
+    if total == number:
+        return True
+    else:
+        return False
+
+
+print(is_armstrong(153))
+print(is_armstrong(9474))
+print(is_armstrong(123))
